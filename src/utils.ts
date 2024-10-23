3c52b5f31f306d8670a04d74fd310c60d39119f6
rednil-rab <bar.linder@gmail.com>
2024-10-23 15:35:50 +0300

    remove unused function from utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,5 @@
 import { toNumber, isNaN } from 'lodash';
 
-export function debounce<F extends (...params: any[]) => void>(fn: F, delay: number) {
-  let timeoutID: number;
-  return function(this: any, ...args: any[]) {
-    clearTimeout(timeoutID);
-    timeoutID = window.setTimeout(() => fn.apply(this, args), delay);
-  } as F;
-}
-
 export function hideLetters (str: string): string {
   if (str === undefined) {
     return '';
@@ -15,9 +7,9 @@ export function hideLetters (str: string): string {
   return str.split('').map(char => Math.random() > 0.5 ? '_' : char).join('');
 }
 
-export const setToLocalStorage = (name: string, number: number ) => localStorage.setItem(name, JSON.stringify(number));
+export const setToLocalStorage = (name: string, number: number ): void => localStorage.setItem(name, JSON.stringify(number));
 
-export const getFromLocalStorage = (name: string) => {
+export const getFromLocalStorage = (name: string): number => {
   const numberFromLocalStorage = toNumber(localStorage.getItem(name));
   if (isNaN(numberFromLocalStorage)) return 0;
   return numberFromLocalStorage;
